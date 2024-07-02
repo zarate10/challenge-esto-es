0383b94adf1dc561664a4774c7663d5769140885
zarate10 <zaratelautaro999@gmail.com>
2024-07-02 05:05:33 -0300

    added create user documentation

--- build/routes/user.routes.js
+++ build/routes/user.routes.js
@@ -17,5 +17,30 @@ const router = (0, express_1.Router)();
  *              description: Error interno del servidor.
  */
 router.get('/all', user_controller_1.getAllUsers);
+/**
+ * @swagger
+ * /user/create:
+ *  post:
+ *      summary: Crear usuario.
+ *      tags:
+ *          - Project
+ *      parameters:
+ *          - in: body
+ *            schema:
+ *              type: object
+ *              properties:
+ *                  firstname:
+ *                      type: string
+ *                  lastname:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                      description: En base a este atributo se indexará todo.
+ *      responses:
+ *          200:
+ *              description: Usuario creado correctamente.
+ *          500:
+ *              description: Error interno del servidor.
+ */
 router.post('/create', user_controller_1.createUser);
 exports.default = router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -17,7 +17,31 @@ const router = Router();
  *              description: Error interno del servidor.
  */
 router.get('/all', getAllUsers)
-
+/**
+ * @swagger 
+ * /user/create:
+ *  post: 
+ *      summary: Crear usuario.
+ *      tags: 
+ *          - Project
+ *      parameters: 
+ *          - in: body
+ *            schema:  
+ *              type: object
+ *              properties: 
+ *                  firstname: 
+ *                      type: string
+ *                  lastname:
+ *                      type: string
+ *                  email:
+ *                      type: string
+ *                      description: En base a este atributo se indexará todo. 
+ *      responses:
+ *          200: 
+ *              description: Usuario creado correctamente. 
+ *          500: 
+ *              description: Error interno del servidor.
+ */
 router.post('/create', createUser)
 
 export default router; 
